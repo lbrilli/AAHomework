a7c29c5c226b695baad3fccf1c98459e65cbf221
lbrilli <lbrilli11@gmail.com>
2021-06-01 08:52:40 -0600

    Assignment 2 with 1 hr of work

---
+++ W4D2/Mancala/lib/board.rb
@@ -0,0 +1,55 @@
+class Board
+  attr_accessor :cups
+
+  def initialize(name1, name2)
+    @cups = Array.new(14) {Array.new}
+    place_stones
+  end
+
+  def place_stones
+    # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, idx|
+      if idx >= 0 && idx < 6 || idx > 6 && idx < 13
+        4.times do
+          cup << :stone
+        end
+      end
+    end
+
+  end
+
+  def valid_move?(start_pos)
+    raise "Invalid starting cup" if start_pos < 0 || start_pos > 12
+    raise "Starting cup is empty" if @cups[start_pos].empty?
+  end
+
+  def make_move(start_pos, current_player_name)
+    moves = @cups[start_pos].length
+    i = start_pos
+    while i > start_pos && i <= moves + start_pos
+      if i != 6 && i != 14
+        @cups[i] << @cups[start_pos].shift
+      end
+      i += 1
+    end
+
+  end
+
+  def next_turn(ending_cup_idx)
+    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+  end
+
+  def render
+    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    puts ""
+    puts ""
+  end
+
+  def one_side_empty?
+  end
+
+  def winner
+  end
+end
