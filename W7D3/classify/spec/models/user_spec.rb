f9619d337bea8d67e9845c0c20ff699846400def
lbrilli <lbrilli11@gmail.com>
2021-06-23 11:21:30 -0600

    more test

--- W7D3/classify/spec/models/user_spec.rb
+++ W7D3/classify/spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  subject(:user) {User.new(email: "AATester@appacademy.io", password: "hunter12")}
+  subject(:user) {User.new(email: "lbrilli11", password: "123456")}
   
   it {should validate_presence_of(:email)}
   it {should validate_presence_of(:password_digest)}
@@ -10,7 +10,7 @@ RSpec.describe User, type: :model do
 
   describe "#is_password?"
     it "checks if the password given matches the digest in the database" do
-      expect(user.is_password?("hunter12")).to be true
+      expect(user.is_password?("123456")).to be true
     end
 
     it "checks if the password given does not match the digest in the databse" do
@@ -18,10 +18,19 @@ RSpec.describe User, type: :model do
     end
 
   describe "reset_session_token!"
-    it "assigns the user a new session token"
+    it "assigns the user a new session token" do
       last_session_token = user.session_token
       user.reset_session_token!
       expect(last_session_token).to_not eq(user.session_token)
+    end
+
+  describe "::find_by_credentials"
+    it "returns a user when a match is found" do
+      expect(User.find_by_credentials("lbrilli11","123456")).to eq(user)
+    end
+
+    it "returns nil when a match is not found" do
+      expect(User.find_by_credentials("AATester@appacademy.io","hunter127")).to_not eq(user)
     end
 
 end
