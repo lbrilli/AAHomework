5322aea1ad8de6246744c47eda845120029843a8
lbrilli <lbrilli11@gmail.com>
2021-06-30 08:35:39 -0600

    shell creation

---
+++ W8D3/Untitled-1.js
@@ -0,0 +1,15 @@
+let newArray = []
+
+function titleize (names, callback) {
+    names.map(ele => )
+}
+
+
+function printCallback (newArray) {
+    arr.forEach(ele => console.log(ele))
+}
+
+
+let arr = ["Mary", "Brian", "Leo"];
+
+titleize(["Mary", "Brian", "Leo"], printCallback);
